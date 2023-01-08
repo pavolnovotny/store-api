47c72b9264eb7678889eb5d4fe90727ea8624a91
Pavol Novotny <pavol.novotny@pavols-laptop.home>
2023-01-08 19:08:56 +0100

    Set app

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('04 Store API')
+import express from 'express'
+import * as dotenv from 'dotenv'
+
+import {notFound} from './middleware/not-found.js'
+import {errorHandlerMiddleware} from './middleware/error-handler.js'
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+  try {
+    //   connect DB
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = import('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
