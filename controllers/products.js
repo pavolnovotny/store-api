3314deaebd7e9feb4065372d710e199a2f66fd4d
Pavol Novotny <pavol.novotny@pavols-laptop.home>
2023-01-10 20:17:57 +0100

    sorting

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,37 @@
 import express from "express"
+import Product from "../models/product.js";
 
 export const getAllProductsStatic = async (req,res) => {
-  res.status(200).json({msg: 'products testing route'})
+  const products = await Product.find({})
+    .sort('-name price')
+  res.status(200).json({
+    products,
+    nbHits: products.length
+  })
 }
 
 export const getAllProducts = async (req,res) => {
-  res.status(200).json({msg: 'products route'})
+  const { featured, company, name, sort } = req.query
+  const queryObject = {}
+
+  if (featured) queryObject.featured = featured === 'true' ? true : false
+
+  if (company) queryObject.company = company
+
+  if (name) queryObject.name = {$regex: name, $options: 'i'}
+
+  let result = Product.find((queryObject))
+
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+
+  const products = await result
+
+  console.log(req.query)
+  res.status(200).json({products, nbHits: products.length})
 }
 
